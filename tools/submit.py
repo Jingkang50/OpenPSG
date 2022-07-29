0a4b8f6d4ca36cebaf908bd96cc807e09ec16da5
king159 <zhuce70@gmail.com>
2022-07-29 16:11:30 +0800

    write to text

--- tools/submit.py
+++ tools/submit.py
@@ -1,15 +1,23 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import argparse
+import os
 
 from mmcv import Config
 from openpsg.datasets import build_dataset
 from submit_result import load_results
 
-evaluation = dict(metric=['sgdet'],
+evaluation1 = dict(metric=['sgdet'],
                   relation_mode=True,
                   classwise=True,
                   iou_thrs=0.5,
                   detection_method='pan_seg')
+
+evaluation2 = dict(metric=['PQ'],
+                  relation_mode=True,
+                  classwise=True,
+                  iou_thrs=0.5,
+                  detection_method='pan_seg')
+
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -29,15 +37,19 @@ def main():
     dataset = build_dataset(cfg.data.test)
     outputs = load_results(args.input_path)
 
-    eval_kwargs = evaluation
+    eval_kwargs = evaluation1
     metric = dataset.evaluate(outputs, **eval_kwargs)
-    print(metric)
+    output_filename = os.path.join(args.output_dir, 'scores.txt')
+    with open(output_filename, 'w') as f3:
+        f3.write('Recall R 20: {}\n'.format(metric['sgdet_recall_R_20']))
+        f3.write('MeanRecall R 20: {}\n'.format(metric['sgdet_mean_recall_mR_20']))
 
-    # TODO:print result to output_path(args.output_path)...
-
-    # metric_dict = dict(config=args.config, metric=metric)
-    # if args.work_dir is not None:
-    #     mmcv.dump(metric_dict, json_file)
+    eval_kwargs = evaluation2
+    metric = dataset.evaluate(outputs, **eval_kwargs)
+    output_filename = os.path.join(args.output_dir, 'scores.txt')
+    with open(output_filename, 'w') as f3:
+        f3.write('PQ: {}\n'.format(metric['PQ']))
+        f3.write('Final Score: {}\n'.format(metric['sgdet_recall_R_20'] * 0.3 + metric['sgdet_mean_recall_mR_20'] * 0.6 + 0.1 * metric['PQ']))
 
 
 if __name__ == '__main__':
