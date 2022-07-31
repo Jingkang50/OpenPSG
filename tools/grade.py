1c7952e8471e88ed673f7938e12e764b26cbea43
Jingkang Yang <jingkang001@e.ntu.edu.sg>
2022-07-31 15:51:32 +0800

    local debug

--- tools/grade.py
+++ tools/grade.py
@@ -1,17 +1,19 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import argparse
+import json
 import os
 import random
-import json
 
-from mmcv import Config
-from openpsg.datasets import build_dataset
-from PIL import Image
 import numpy as np
 import PIL
-from openpsg.models.relation_heads.approaches import Result
-from panopticapi.utils import rgb2id
+from mmcv import Config
 from mmdet.datasets.coco_panoptic import INSTANCE_OFFSET
+from panopticapi.utils import rgb2id
+from PIL import Image
+
+from openpsg.datasets import build_dataset
+from openpsg.models.relation_heads.approaches import Result
+
 
 def save_results(results):
     all_img_dicts = []
@@ -26,21 +28,18 @@ def save_results(results):
         segments_info = []
         img = np.full(masks.shape[1:3], 0)
         for label, mask in zip(labels, masks):
-            r,g,b = random.choices(range(0,255), k=3)
-            coloring_mask = 1 * np.vstack([[mask]]*3)
-            for j, color in enumerate([r,g,b]):
-                coloring_mask[j,:,:] = coloring_mask[j,:,:] * color
+            r, g, b = random.choices(range(0, 255), k=3)
+            coloring_mask = 1 * np.vstack([[mask]] * 3)
+            for j, color in enumerate([r, g, b]):
+                coloring_mask[j, :, :] = coloring_mask[j, :, :] * color
             img = img + coloring_mask
 
-            segment = dict(
-                category_id=int(label),
-                id=rgb2id((r,g,b))
-            )
+            segment = dict(category_id=int(label), id=rgb2id((r, g, b)))
             segments_info.append(segment)
 
-        image_path = 'submission/panseg/%d.png'%idx
+        image_path = 'submission/panseg/%d.png' % idx
         # image_array = np.uint8(img).transpose((2,1,0))
-        image_array = np.uint8(img).transpose((1,2,0))
+        image_array = np.uint8(img).transpose((1, 2, 0))
         PIL.Image.fromarray(image_array).save(image_path)
 
         single_result_dict = dict(
@@ -55,13 +54,14 @@ def save_results(results):
         json.dump(all_img_dicts, outfile, default=str)
 
 
-def load_results(filename):
-    with open(filename) as infile:
+def load_results(loadpath):
+    with open(os.path.join(loadpath, 'relation.json')) as infile:
         all_img_dicts = json.load(infile)
 
-    results=[]
+    results = []
     for single_result_dict in all_img_dicts:
         pan_seg_filename = single_result_dict['pan_seg_file_name']
+        pan_seg_filename = os.path.join(loadpath, 'panseg', pan_seg_filename)
         pan_seg_img = np.array(Image.open(pan_seg_filename))
         pan_seg_img = pan_seg_img.copy()  # (H, W, 3)
         seg_map = rgb2id(pan_seg_img)
@@ -74,24 +74,25 @@ def load_results(filename):
         masks = []
         for _, s in enumerate(segments_info):
             label = s['category_id']
-            labels.append(label) #TODO:1-index for gt?
+            labels.append(label)  #TODO:1-index for gt?
             masks.append(seg_map == s['id'])
 
         count = dict()
         pan_result = seg_map.copy()
         for _, s in enumerate(segments_info):
-            label = s['category_id']
+            label = int(s['category_id'])
             if label not in count.keys():
                 count[label] = 0
-            pan_result[seg_map == s['id']] = label-1 + count[label] * INSTANCE_OFFSET #change index?
-            count[label]+=1
+            pan_result[seg_map == s['id']] = label - 1 + count[
+                label] * INSTANCE_OFFSET  #change index?
+            count[label] += 1
 
         rel_array = np.asarray(single_result_dict['relations'])
         if len(rel_array) > 20:
             rel_array = rel_array[:20]
         rel_dists = np.zeros((len(rel_array), 57))
         for idx_rel, rel in enumerate(rel_array):
-            rel_dists[idx_rel, rel[2]] += 1 # TODO:1-index for gt?
+            rel_dists[idx_rel, rel[2]] += 1  # TODO:1-index for gt?
 
         result = Result(
             rels=rel_array,
@@ -103,36 +104,39 @@ def load_results(filename):
             pan_results=pan_result,
         )
         results.append(result)
-    
+
     return results
 
+
 def parse_args():
-    parser = argparse.ArgumentParser(
-        description='MMDet eval a model')
-    parser.add_argument('config', help='config file path') # configs/_base_/datasets/psg_val.py
+    parser = argparse.ArgumentParser(description='MMDet eval a model')
     parser.add_argument('input_path', help='input file path')
     parser.add_argument('output_path', help='output file path')
-    
+
     args = parser.parse_args()
     return args
 
 
 def main():
     args = parse_args()
-    cfg = Config.fromfile(args.config)
+    cfg = Config.fromfile('configs/_base_/datasets/psg_val.py')
 
     dataset = build_dataset(cfg.data.test)
     outputs = load_results(args.input_path)
     metric1 = dataset.evaluate(outputs, **cfg.evaluation1)
     metric2 = dataset.evaluate(outputs, **cfg.evaluation2)
-    
+
     output_filename = os.path.join(args.output_path, 'scores.txt')
-    
+
     with open(output_filename, 'w') as f3:
         f3.write('Recall R 20: {}\n'.format(metric1['sgdet_recall_R_20']))
-        f3.write('MeanRecall R 20: {}\n'.format(metric1['sgdet_mean_recall_mR_20']))
+        f3.write('MeanRecall R 20: {}\n'.format(
+            metric1['sgdet_mean_recall_mR_20']))
         f3.write('PQ: {}\n'.format(metric2['PQ']))
-        f3.write('Final Score: {}\n'.format(metric1['sgdet_recall_R_20'] * 0.3 + metric1['sgdet_mean_recall_mR_20'] * 0.6 + 0.001 * metric2['PQ']))
+        f3.write(
+            'Final Score: {}\n'.format(metric1['sgdet_recall_R_20'] * 0.3 +
+                                       metric1['sgdet_mean_recall_mR_20'] *
+                                       0.6 + 0.001 * metric2['PQ']))
 
 
 if __name__ == '__main__':
